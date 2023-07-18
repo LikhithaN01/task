cca7f9c5e59fe8eb79f1a1d6d7ed7bbf81f7968b
LikhithaN01 <139860561+LikhithaN01@users.noreply.github.com>
2023-07-18 15:27:28 +0530

    Add files via upload

---
+++ dbtask.js
@@ -0,0 +1,67 @@
+const mysql = require("mysql")
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "DEMO"
+})
+con.connect();
+con.query('SELECT *FROM EMPLOYEE', function (error, results, fields) {
+    if (error) throw error;
+    
+     console.log('The solution is: ', results);
+        
+});
+const record = {
+    ID : "A19" ,
+    dNAME : "sandy",
+    SALARY : 33000,
+    AGE :25
+};
+const insertQuery = 'INSERT INTO EMPLOYEE SET ?';
+con.query(insertQuery,record,(error, results)=>{
+    if (error) throw error;
+    
+     console.log('Record inserted successfully');
+     console.log('INSERTD RECORD ID ', results.record);
+
+})
+
+const deleteQuery = 'DELETE FROM EMPLOYEE WHERE ID = "A19"';
+con.query(deleteQuery,(error, results)=>{
+    if (error) throw error;
+    
+     console.log('Deleted successfully');
+     console.log(' ', results.deleteQuery);
+})
+con.end();
+
+
+
+
+// con.connect((err)=>{
+//     if(err)throw err;
+//     else
+//     console.log("database is connected")
+// })
+// 'use strict';
+
+// const mysqlx = require('@mysql/xedvapi');
+// const options = { user: 'root', password: 'CloudPlinth@123', schema: 'demo' };
+
+// (async function () {
+//   let session;
+
+//   try {
+//     session = await mysqlx.getSession(options);
+
+//     const collection = await session.getSchema(options.schema).createCollection('myCollection');
+//     await collection.add({ name: 'foo' }).execute();
+//     await collection.find().fields('name').execute(console.log); // { name: 'foo' }
+
+//   } catch (err) {
+//     console.error(err.message);
+//   } finally {
+//     session && session.close();
+//   }
+// })();
